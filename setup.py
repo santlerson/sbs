87f079125e3f98539cdfb866224af4c2457b5062
santlerson <shmooseyantlerson@gmail.com>
2021-10-11 11:41:44 +0300

    Added metadata, license and setup.py, build wheel

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sbs-santlerson",
+    version="1.1.1",
+    author="Shmoosey Antlerson",
+    author_email="shmooseyantlerson@gmail.com",
+    description="A tool for creating secure and encrypted backups on Google Drive",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/santlerson/sbs",
+    project_urls={
+        "Bug Tracker": "https://github.com/santlerson/sbs/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': ['sbs=src.sbs:main'],
+    }
+)
